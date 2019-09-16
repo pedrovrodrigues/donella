151a8dc94bba587ab5bf2a0906c1987246f8ad74
Eduardo Alarcon <eduambarbosa@gmail.com>
2019-09-16 17:34:45 -0300

    Adding new pages

--- donella.py
+++ donella.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
+    return render_template('home.html')
+
+@app.route('/donella-dnd')
+def donella_dnd():
     return render_template('donella-dnd.html')
+
+@app.route('/donella-code')
+def donella_code():
+    return render_template('donella.html')
 
 if __name__ == '__main__':
     app.run()
